c5b7138c854ffbe38b4fbbc24d8fe647a08e9d80
Aksel89 <denetor89@yandex.ru>
2021-12-16 01:47:57 +0300

    lesson 3 start

--- TODOnotes/TODOnotes/urls.py
+++ TODOnotes/TODOnotes/urls.py
@@ -21,6 +21,7 @@ from TODOnotes.users.views import UserViewSet
 router = DefaultRouter()
 router.register('users', UserViewSet)
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
