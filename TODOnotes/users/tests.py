e3ea265d70bd11a4308a98f15cf258436f419975
Aksel89 <denetor89@yandex.ru>
2022-01-18 00:50:29 +0300

    lesson 8 done

--- TODOnotes/users/tests.py
+++ TODOnotes/users/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate
+from .models import Users
+from .views import UserViewSet
 
-# Create your tests here.
+
+class TestUserViewSet(TestCase):
+
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/users/')
+        view = UserViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_admin(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/users/')
+        admin = Users.objects.create_superuser('admin', 'admin@examle.com', '123')
+        force_authenticate(request, admin)
+        view = UserViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
